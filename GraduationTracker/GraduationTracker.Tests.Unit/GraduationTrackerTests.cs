211c58e8f0f769c3415c9165449d8906f1baaf60
Darren Laser <darren_laser@hotmail.com>
2018-08-19 16:47:40 -0400

    Added repository unit tests. Fixed initial issue changing Assert.IsTrue to Assert.IsFalse. Added an additional integration test to determine if diploma graduation can be failed.

--- GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
+++ GraduationTracker/GraduationTracker.Tests.Unit/GraduationTrackerTests.cs
@@ -9,67 +9,19 @@ namespace GraduationTracker.Tests.Unit
     public class GraduationTrackerTests
     {
         [TestMethod]
-        public void TestHasCredits()
+        public void IntegrationTestHasCredits()
         {
-            var tracker = new GraduationTracker();
+						var tracker = new GraduationTracker();
 
-            var diploma = new Diploma
-            {
-                Id = 1,
-                Credits = 4,
-                Requirements = new int[] { 100, 102, 103, 104 }
-            };
-
-            var students = new[]
-            {
-               new Student
-               {
-                   Id = 1,
-                   Courses = new Course[]
-                   {
-                        new Course{Id = 1, Name = "Math", Mark=95 },
-                        new Course{Id = 2, Name = "Science", Mark=95 },
-                        new Course{Id = 3, Name = "Literature", Mark=95 },
-                        new Course{Id = 4, Name = "Physichal Education", Mark=95 }
-                   }
-               },
-               new Student
-               {
-                   Id = 2,
-                   Courses = new Course[]
-                   {
-                        new Course{Id = 1, Name = "Math", Mark=80 },
-                        new Course{Id = 2, Name = "Science", Mark=80 },
-                        new Course{Id = 3, Name = "Literature", Mark=80 },
-                        new Course{Id = 4, Name = "Physichal Education", Mark=80 }
-                   }
-               },
-            new Student
-            {
-                Id = 3,
-                Courses = new Course[]
-                {
-                    new Course{Id = 1, Name = "Math", Mark=50 },
-                    new Course{Id = 2, Name = "Science", Mark=50 },
-                    new Course{Id = 3, Name = "Literature", Mark=50 },
-                    new Course{Id = 4, Name = "Physichal Education", Mark=50 }
-                }
-            },
-            new Student
-            {
-                Id = 4,
-                Courses = new Course[]
-                {
-                    new Course{Id = 1, Name = "Math", Mark=40 },
-                    new Course{Id = 2, Name = "Science", Mark=40 },
-                    new Course{Id = 3, Name = "Literature", Mark=40 },
-                    new Course{Id = 4, Name = "Physichal Education", Mark=40 }
-                }
-            }
-
-
-            //tracker.HasGraduated()
-        };
+						var diploma = Repository.GetDiploma(1);
+						
+						var students = new[]
+						{
+								Repository.GetStudent(1),
+								Repository.GetStudent(2),
+								Repository.GetStudent(3),
+								Repository.GetStudent(4)
+						};
             
             var graduated = new List<Tuple<bool, STANDING>>();
 
@@ -77,12 +29,34 @@ namespace GraduationTracker.Tests.Unit
             {
                 graduated.Add(tracker.HasGraduated(diploma, student));      
             }
-
-            
-            Assert.IsFalse(graduated.Any());
+						
+            Assert.IsTrue(graduated.Any());
 
         }
 
+				[TestMethod]
+				public void IntegrationTestGraduationFailure()
+				{
+						var tracker = new GraduationTracker();
 
-    }
+						var diploma = Repository.GetDiploma(1);
+
+						var students = new[]
+						{
+								Repository.GetStudent(1),
+								Repository.GetStudent(2),
+								Repository.GetStudent(3),
+								Repository.GetStudent(4)
+						};
+
+						var graduated = new List<Tuple<bool, STANDING>>();
+
+						foreach (var student in students) {
+								graduated.Add(tracker.HasGraduated(diploma, student));
+						}
+
+						Assert.IsFalse(graduated.Any(m => m.Item2 == STANDING.None));
+
+				}
+		}
 }
---
+++ GraduationTracker/GraduationTracker.Tests.Unit/RepositoryTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraduationTracker.Tests.Unit
+{
+		//The following are unit tests for the repository class
+		[TestClass]
+		public class RepositoryTests
+		{
+				[TestMethod]
+				public void UnitTestIsStudentValid(){
+						var s = Repository.GetStudent(3);
+						Assert.IsTrue(s != null);
+						Assert.IsTrue(s.Courses.Any(m => m.Mark >= 0));
+						Assert.IsTrue(s.Courses.Any(m => m.Mark <= 100));
+				}
+				[TestMethod]
+				public void UnitTestIsDiplomaValid()
+				{
+						var d = Repository.GetDiploma(1);
+						Assert.IsTrue(d != null);
+						Assert.IsTrue(d.Credits > 0);
+
+				}
+				[TestMethod]
+				public void UnitTestIsRequirementValid()
+				{
+						var r = Repository.GetRequirement(102);
+						Assert.IsTrue(r != null);
+						Assert.IsTrue(r.MinimumMark > 49);
+				}
+		}
+}
